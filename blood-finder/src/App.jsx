d6bc109210291838f22fe64099aab09ff7e90b69
nahid383 <nahid1719abd@gmail.com~>
2026-08-09 20:57:08 +0600

    initial commit

---
+++ blood-finder/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="bg-slate-800 min-h-screen">
+      <div className="container mx-auto py-8">
+        <Header/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ blood-finder/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+        return (
+            <div className="card bg-slate-900 shadow-sm">
+          <div className="card-body">
+            <h2 className="font-bold text-3xl text-white">
+              BloodFinder
+            </h2>
+
+            <p className="text-slate-300">
+              Donor registry, search, request management, and AI matching score.
+            </p>
+          </div>
+        </div>
+        );
+    
+}
+
+export default Header;
